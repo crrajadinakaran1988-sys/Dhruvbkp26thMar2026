78782cb8ec72f8f5ae3e0601479a1f5b4ce13959
labuser <labuser@ip-172-31-36-215.ap-south-1.compute.internal>
2026-03-27 20:37:03 +0000

    Added on 26thmar2026 with sql queries

---
+++ exp_management_app/SQLqueries.sql
@@ -0,0 +1,8 @@
+Create Database expense_db;
+use expense_db;
+
+Create table expenses
+(
+title varchar(100), 
+amount DECIMAL(10,2)
+)
--- exp_management_app/config/db.js
+++ exp_management_app/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'admin@123',
+    password: 'root123$',
     database: 'expense_db'
 });
 
